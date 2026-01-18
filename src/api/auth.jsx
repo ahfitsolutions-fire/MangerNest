ef8538c40553d4c6448c4b54aaab689af7d8805a
Sheetal <ahfitsolutions@gmail.com>
2026-01-18 18:15:55 +0530

    updated

--- src/api/auth.jsx
+++ src/api/auth.jsx
@@ -1,9 +1,17 @@
+export const signupUser = async (data) => {
+  const res = await fetch("https://api.escuelajs.co/api/v1/users/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  return res.json();
+};
+
 export const loginUser = async (data) => {
   const res = await fetch("https://api.escuelajs.co/api/v1/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
-
   return res.json();
 };
